3ed3497d63f6ddcb16f85113caec30ebe9eb5328
KILBA <kilba2077@gmail.com>
2025-09-17 09:42:33 +0530

    Update HelloEveryone.java

--- HelloEveryone.java
+++ HelloEveryone.java
@@ -1,5 +1,5 @@
 public class HelloEveryone{
   public static void main(String args[]){
-    system.out.println("HelloEveryone! Its my first time using Jenkins.");
+    System.out.println("HelloEveryone! Its my first time using Jenkins.");
   }
 }
