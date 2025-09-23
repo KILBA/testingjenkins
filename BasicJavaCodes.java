a313fbdc19f24cb06dcbdfece48ce0522082023e
KILBA <kilba2077@gmail.com>
2025-09-23 21:45:15 +0530

    Create BasicJavaCodes.java

---
+++ BasicJavaCodes.java
@@ -0,0 +1,8 @@
+public class BasicJavaCodes {
+  public static void main(String[] args) {
+    int a = 10, b = 20, ans;
+    ans = a + b;
+    System.out.println("Hello World");
+    System.out.println("This is addition of "+a+"+"+b+"="+ans);
+  }
+}
