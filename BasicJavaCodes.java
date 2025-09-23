03afbe1a669bef4a074b87fc9813314e999c929e
KILBA <kilba2077@gmail.com>
2025-09-23 23:34:51 +0530

    Update BasicJavaCodes.java

--- BasicJavaCodes.java
+++ BasicJavaCodes.java
@@ -1,8 +1,10 @@
 public class BasicJavaCodes {
   public static void main(String[] args) {
-    int a = 10, b = 20, ans;
+    int a = 10, b = 20, ans, sub;
     ans = a + b;
+    sub = b-a;
     System.out.println("Hello World");
     System.out.println("This is addition of "+a+"+"+b+"="+ans);
+    System.out.println("This is subtraction of "+b+"-"+a+"="+sub);
   }
 }
